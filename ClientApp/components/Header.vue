17c7501b1fa570c50bcc8b34d04d33f5c7e46ec1
Sanghyeon <sanghyeon@dalsoft.co.kr>
2019-04-16 17:35:07 +0900

    헤더 작성중
    헤더 작성중

---
+++ ClientApp/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header-container">
+    <nav class="tab-navi">
+      <div class="menu">집</div>
+      <div class="menu">NEW</div>
+      <div class="menu">나</div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+  .header-container {
+    width: 100%;
+    height: 100px;
+  }
+</style>
--- ClientApp/views/app-root.vue
+++ ClientApp/views/app-root.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
-    <header params="route: route"></header>
+  <div id="app" class="container-fluid">
+    <!-- <header params="route: route"></header> -->
+    <div>
+      <header></header>
+      <!-- <nav params="route: route"></nav> -->
+    </div>
 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import nav from '../components/nav-menu'
   import Header from '../components/Header.vue'
 
   export default {
     components: {
       Header,
+      nav,
     },
 
     data() {
@@ -21,4 +27,7 @@
 </script>
 
 <style>
+  #app {
+    height: 100%;
+  }
 </style>
