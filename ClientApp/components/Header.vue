0cddd84c8dd5b192f64f7b941b17e50bdec05f3d
Sanghyeon <sanghyeon@dalsoft.co.kr>
2019-04-17 19:06:44 +0900

    헤더 제작중
    헤더 제작중

--- ClientApp/components/Header.vue
+++ ClientApp/components/Header.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="header-container">
-    <div></div>
-    <nav class="tab-navi">
-      <router-link :to="route.path" exact-active-class="active" v-for="(route, index) in routes" :key="index" class="menu">
-        <img class="image" :src="$route | iconFilter(route)" :alt="route.display">
-      </router-link>
-    </nav>
+    <div v-if="mainHeader" class="header__main">
+      <div class="search-area">
+        <img class="search-icon" src="../assets/image/btn_search.png" alt="검색" @click="searchClickHandler">
+      </div>
+      <nav class="tab-navi">
+        <router-link :to="route.path" exact-active-class="active" v-for="(route, index) in routes" :key="index" class="menu">
+          <img class="image" :src="$route | iconFilter(route)" :alt="route.display">
+        </router-link>
+      </nav>
+    </div>
+
+    <div v-else class="header__search"></div>
   </div>
 </template>
 
@@ -21,42 +27,64 @@
     data() {
       return {
         routes,
+        mainHeader: true,
       }
     },
-    methods: {},
+    methods: {
+      searchClickHandler() {
+        // 검색!
+        this.mainHeader = false
+      },
+    },
   }
 </script>
 
 <style lang="scss" scoped>
   .header-container {
     width: 100%;
-    .tab-navi {
-      display: flex;
-      justify-content: center;
-      font-size: 10px;
-      height: 42px;
-    }
-    .menu {
-      width: 50px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      &:not(:last-child) {
-        margin-right: 15px;
-      }
-      .image {
-        width: 24px;
-        height: 24px;
-      }
-      &:nth-of-type(2) {
-        .image {
-          width: 20px;
-          height: 16px;
+    .header__main {
+      width: 100%;
+
+      .search-area {
+        width: 100%;
+        text-align: right;
+        .search-icon {
+          width: 24px;
+          height: 24px;
         }
       }
+      .tab-navi {
+        display: flex;
+        justify-content: center;
+        font-size: 10px;
+        height: 42px;
+      }
+      .menu {
+        width: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &:not(:last-child) {
+          margin-right: 15px;
+        }
+        .image {
+          width: 24px;
+          height: 24px;
+        }
+        &:nth-of-type(2) {
+          .image {
+            width: 20px;
+            height: 16px;
+          }
+        }
+      }
+      .active {
+        border-bottom: 2px solid #ffaa00;
+      }
     }
-    .active {
-      border-bottom: 2px solid #ffaa00;
+
+    .header__search {
+      width: 100%;
     }
   }
 </style>
